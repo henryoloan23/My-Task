713e209c09965e3f6394827f5236fbd7806ba0c1
Henry Oloan Tagor Harahap <henryoloan2@gmail.com>
2024-09-10 19:09:08 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,75 @@
+# 1_penjumlahan
+print('penjumlahan')
+bil1 = int(input('masukkan bilangan pertama : '))
+# misal 20
+bil2 = int(input('masukkan bilangan kedua nya : '))
+# misal 30
+hasil = bil1 + bil2
+print(hasil)
+# hasil nya 50
+
+# 2_pengurangan
+print('pengurangan')
+bil1 = int(input('masukkan bilangan pertama : '))
+# misal 20
+bil2 = int(input('masukkan bilangan kedua : '))
+# misal 10
+hasil = bil1 - bil2
+print(hasil)
+# hasil nya 10
+
+# 3_perkalian
+print('perkalian')
+bil1 = int(input('masukkan bilangan pertama :'))
+# misal 10
+bil2 = int(input('masukkan bilangan kedua nya : '))
+# misal 10
+hasil = bil1 * bil2
+print(hasil)
+# hasil nya 100 
+
+# 4_pangkat
+print('perpangkatan')
+bil1 = int(input('masukkan bilangan pertama :'))
+# misal 2
+bil2 = int(input('masukkan bilangan kedua nya : '))
+# misal 3
+hasil = bil1 ** bil2
+print(hasil)
+# hasil nya 8
+
+# 5_integer division
+print('integer division')
+bil1 = float(input('masukkan bilangan pertama : '))
+# misal 10.2
+bil2 = float(input('masukkan bilangan kedua : '))
+# misal 12.5
+hasil1 = int(bil1)
+hasil2 = int(bil2)
+print(hasil1)
+# hasil1 adalah 10
+print(hasil2)
+# hasil2 adalah 12
+
+# 6_floating division
+print('floating division')
+bil1 = int(input('masukkan bilangan pertama : '))
+# misal 20
+bil2 = int(input('masukkan bilangan kedua : '))
+# misal 25
+hasil1 = float(bil1)
+hasil2 = float(bil2)
+print(hasil1)
+# hasil1 adalah 20.0
+print(hasil2)
+# hasil2 adalah 25.0
+
+# 7_modulo
+print('modulo')
+bil1 = int(input('masukkan bilangan pertama : '))
+# misal 10
+bil2 = int(input('masukkan bilangan kedua : '))
+# misal 3
+hasil = bil1 % bil2
+print(hasil)
+# hasil nya adalah 1
